0754918cc05aa4e1b9cc396c51764fbef3f538fd
JoaquinHaedo <jhaedo2000@gmail.com>
2022-09-25 18:05:38 -0300

    Update teacherClasses.tsx

--- frontend/pages/teacherClasses.tsx
+++ frontend/pages/teacherClasses.tsx
@@ -7,7 +7,8 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import PublishIcon from '@mui/icons-material/Publish';
 import DownloadIcon from '@mui/icons-material/Download';
-import { Button } from '@mui/material';
+import { Button} from '@mui/material';
+import IconButton from '@mui/material/IconButton'
 import Switch from '@mui/material/Switch';
 
 
@@ -28,54 +29,55 @@ const columns: GridColDef[] = [
     sortable: false,
     width: 160,
     //valueGetter: (params: GridValueGetterParams) =>
-      //`${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    //`${params.row.firstName || ''} ${params.row.lastName || ''}`,
   },
-  { field: 'status', headerName: 'Publicado', width: 100},
+  { field: 'status', headerName: 'Publicado', width: 100 },
+  { field: 'actions', headerName: 'Acciones', renderCell:()=>{return <div><IconButton color="error"><DeleteIcon/></IconButton> <IconButton color="secondary"><EditIcon/></IconButton></div>}}
 ];
 
 const rows = [
-  { id: 1, className: 'Snow', matter: 'Jon', duration: 35, frecuency: 'Semanal', status: <Switch /> },
-  { id: 2, className: 'Lannister', matter: 'Cersei', duration: 42 , frecuency: 'diaria', status: 'Publicada' },
-  { id: 3, className: 'Lannister', matter: 'Jaime', duration: 45, frecuency: 'diaria', status: 'Sin publicar'  },
-  { id: 4, className: 'Stark', matter: 'Arya', duration: 16 , frecuency: 'Semanal', status: 'Publicada' },
-  { id: 5, className: 'Targaryen', matter: 'Daenerys', duration: null , frecuency: 'Semanal', status: 'Sin publicar' },
-  { id: 6, className: 'Melisandre', matter: null, duration: 150 , frecuency: 'Semanal', status: 'Sin publicar' },
-  { id: 7, className: 'Clifford', matter: 'Ferrara', duration: 44 , frecuency: 'diaria', status: 'Publicada' },
-  { id: 8, className: 'Frances', matter: 'Rossini', duration: 36 , frecuency: 'Semanal', status: 'Publicada' },
-  { id: 9, className: 'Roxie', matter: 'Harvey', duration: 65 , frecuency: 'Semanal', status: 'Publicada' },
+  Row(1,'Snow','Jon',35,'Semanal', '' ),
+  { id: 2, className: 'Lannister', matter: 'Cersei', duration: 42, frecuency: 'diaria', status: 'Publicada' },
+  { id: 3, className: 'Lannister', matter: 'Jaime', duration: 45, frecuency: 'diaria', status: 'Sin publicar' },
+  { id: 4, className: 'Stark', matter: 'Arya', duration: 16, frecuency: 'Semanal', status: 'Publicada' },
+  { id: 5, className: 'Targaryen', matter: 'Daenerys', duration: null, frecuency: 'Semanal', status: 'Sin publicar' },
+  { id: 6, className: 'Melisandre', matter: null, duration: 150, frecuency: 'Semanal', status: 'Sin publicar' },
+  { id: 7, className: 'Clifford', matter: 'Ferrara', duration: 44, frecuency: 'diaria', status: 'Publicada' },
+  { id: 8, className: 'Frances', matter: 'Rossini', duration: 36, frecuency: 'Semanal', status: 'Publicada' },
+  { id: 9, className: 'Roxie', matter: 'Harvey', duration: 65, frecuency: 'Semanal', status: 'Publicada' },
 ];
+
+function Row(id, className, matter, duration, frecuency, status){
+  return({ id: id, className: className, matter: matter, duration: duration, frecuency: frecuency, status: status, delete: <IconButton color="error"><DeleteIcon/></IconButton>, edit: <IconButton color="secondary"><EditIcon/></IconButton>})
+}
 
 export default function DataTable() {
   return (
     <Container component="main" maxWidth="lg">
-        <CssBaseline />
-            <Box 
-                sx={{
-                    //marginTop: 0,
-                    marginBottom: 20,
-                    paddingBottom:30,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <div style={{ height: 371, width: '100%'}}>
-                    <h1>Mis clases</h1>
-                    <Button variant="outlined">Crear</Button>
-                    <h1></h1>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                />
-                <Button variant="outlined" color="error" startIcon={<DeleteIcon/>}>Eliminar</Button>
-                <Button variant="outlined" color="secondary" startIcon={<EditIcon/>}>Modificar</Button>
-                <Button variant="outlined" startIcon={<DownloadIcon/>}> Despublicar</Button>
-                <Button variant="outlined" color="success" startIcon={<PublishIcon/>}>Publicar</Button>
-                </div>
-            </Box>
+      <CssBaseline />
+      <Box
+        sx={{
+          //marginTop: 0,
+          marginBottom: 20,
+          paddingBottom: 30,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ height: 371, width: '100%' }}>
+          <h1>Mis clases</h1>
+          <Button variant="outlined">Crear</Button>
+          <h1></h1>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            //checkboxSelection
+          />
+        </div>
+      </Box>
     </Container>
   );
 }
