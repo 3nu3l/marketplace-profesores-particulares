b0374ad29930768c0ce6cd23aa4525572b8140cc
Emmanuel Maidana <emma.maidana@gmail.com>
2022-10-08 20:39:33 -0300

    add const port

--- api/index.js
+++ api/index.js
@@ -18,7 +18,8 @@ app.get('/', (req, res) => {
   res.json({ success: true, message: 'Welcome to backend zone!' });
 });
 
-var listener = app.listen(process.env.HTTP_PORT || 3001, () => {
+const PORT = process.env.PORT || 3001;
+var listener = app.listen(PORT, () => {
   console.log(listener.address().port + ' port is listening');
 });
 
