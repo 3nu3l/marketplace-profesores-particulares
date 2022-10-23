828e6ce926d3344ebc19b5941d642c50a2b29ac7
Emmanuel Maidana <emma.maidana@gmail.com>
2022-10-23 14:25:43 -0300

    add regex in password validator

--- api/middlewares/validation/user.js
+++ api/middlewares/validation/user.js
@@ -21,7 +21,9 @@ exports.validateUserSignUp = [
     .trim()
     .not()
     .isEmpty()
-    .withMessage('Password is empty!'),
+    .withMessage('Password is empty!')
+    .matches(/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,16}$/)
+    .withMessage('Ingrese una contraseña válida. Debe poseer: entre 8 y 16 caracteres alfanuméricos, dos letras mayúsculas,un caracter especial, elegir entre: !@#$&*, tres letras minúsculas, dos números'),
   check('role')
     .trim()
     .not()
