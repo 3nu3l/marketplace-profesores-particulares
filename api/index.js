4d995e2691a51e5c10bebe371c92e3c714fc79a9
gordojm <juanma.gordo99@gmail.com>
2022-11-25 16:10:31 -0300

    added cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 require('./models/db');
+var cors = require('cors')
 const userRouter = require('./routes/user');
 const classRouter = require('./routes/class');
 const swaggerUi = require('swagger-ui-express')
@@ -10,11 +11,13 @@ const swaggerJsDoc = require('swagger-jsdoc');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(classRouter);
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
